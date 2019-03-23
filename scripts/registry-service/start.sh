da278f26af9393169d1ee1b029355829e269e79b
root <gabriel.x.ursu@gmail.com>
2019-03-23 16:02:40 +0100

    created the manager deploy blueprint

--- scripts/registry-service/start.sh
+++ scripts/registry-service/start.sh
@@ -2,5 +2,4 @@
 sudo su -
 cd /tmp
 wget https://github.com/OOrsoos/jars/raw/master/registry-service-1.0-SNAPSHOT.jar
-#java -jar /tmp/registry-service-1.0-SNAPSHOT.jar
 nohup java -jar registry-service-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
